4a9c3e182be78a3b3b00b7c34eb3bc18d9e3bb12
DESKTOP-150VU6P\Latha <kkseela01@gmail.com>
2023-06-15 13:18:58 +0100

    utc time

---
+++ CurrentTimeService/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.MapGet("time/utc", () => Results.Ok(DateTime.UtcNow));
+
+await app.RunAsync();
